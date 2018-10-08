c703684f4027b74517d057916fc6fc9e9e513ef8
mohandin <31969602+mohandin@users.noreply.github.com>
2018-10-08 13:38:02 -0400

    Checkin for
    Moved more files to old package.

--- src/mpcTesting/Old/ContactUs.java
+++ src/mpcTesting/Old/ContactUs.java
@@ -1,10 +1,12 @@
-package mpcTesting;
+package mpcTesting.Old;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+
+import mpcTesting.Parameters;
 
 
 public class ContactUs {
